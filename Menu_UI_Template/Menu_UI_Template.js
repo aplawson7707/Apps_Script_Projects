e0de0250b102254ba5520fa04ddd833f188e465a
aplawson7707 <aplawson7707@gmail.com>
2021-06-03 13:10:21 -0700

    update Menu Ui template for V8 Runtime

--- Menu_UI_Template/Menu_UI_Template.js
+++ Menu_UI_Template/Menu_UI_Template.js
@@ -1,6 +1,11 @@
-function onOpen() {
-    const ui = SpreadsheetApp.getUi();
-    ui.createMenu('Top Level')
+/**
+ * The event handler triggered when opening the spreadsheet.
+ * THIS WILL NOT RUN ON OPEN IF THERE ARE VARIABLES OUTSIDE OF FUNCTIONS
+ * @param {Event} e The onOpen event.
+ */
+ function onOpen(e) {
+    SpreadsheetApp.getUi() 
+        .createMenu('Top Level')
         .addItem('Option1', 'function1_name')
         .addItem('Option2', 'function2_name')
         .addToUi();
